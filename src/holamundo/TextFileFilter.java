c2429ddf32d1bd454c39e2e975b8999899b11a36
CabanaMAF <mcab4964@gmail.com>
2023-05-25 23:20:45 -0300

    Mensaje de commit

---
+++ src/holamundo/TextFileFilter.java
@@ -0,0 +1,10 @@
+package holamundo;
+import java.io.File;
+import java.io.FileFilter;
+public class TextFileFilter implements FileFilter {
+ @Override
+ public boolean accept(File pathname) {
+ return pathname.getName().toLowerCase().endsWith(".txt");
+ }
+}
+//se define el tipo de archivo que admitira el indexador
